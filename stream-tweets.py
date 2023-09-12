28796dc0c02095ab0de8619fda502d5f1cd25f3a
sebastian pizarro <sebastian.pizarro@globant.com>
2023-09-11 23:50:20 -0300

    adds TODO to main spark routine to: define tweet schema, import and call notebook functions qX() and display new result

--- stream-tweets.py
+++ stream-tweets.py
@@ -1,6 +1,7 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
+from lib.chalxlenge import *
 
 # Initialize a Spark session
 spark = SparkSession.builder \
@@ -21,8 +22,7 @@ kafka_df = spark.readStream \
     .option("subscribe", kafka_topic) \
     .load()
 ## schema
-
-
+# TODO: create actual tweet schema
 schema = StructType().add("glossary", "string").add("title", "string")
 
 # Assuming you have six aggregation functions q1, q2, q3, q4, q5, and q6
@@ -36,6 +36,12 @@ info_dataframe = base_df.select(
 info_dataframe.printSchema()
 
 info_df_fin = info_dataframe.select("json.*")
+# TODO: import lib module to process this dataframe
+df = info_df_fin.toPandas()
+result = q4(df)
+# TODO: write output to a continuos refreshing matplotlib
+# result.head(10)
+result.plot.pie()
 
 # Define a query to output the results to the console
 query = info_df_fin  \
